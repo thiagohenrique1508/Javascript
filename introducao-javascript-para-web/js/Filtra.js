d443fffa38c401ae2a82d8bec238089f9ea2a029
Thiago Henrique Martins da Silva <thiagohenrique1508@gmail.com>
2020-10-23 09:11:23 -0300

    Projeto Final

--- introducao-javascript-para-web/js/Filtra.js
+++ introducao-javascript-para-web/js/Filtra.js
@@ -1,23 +1,20 @@
 var campoFiltro = document.querySelector("#filtrar-tabela");
 
-campoFiltro.addEventListener("input", function(){
-
+campoFiltro.addEventListener("input", function() {
     var pacientes = document.querySelectorAll(".paciente");
 
-    if (this.value.length > 0){
-        for (let i = 0; i < pacientes.length; i++) {
-            var paciente = pacientes[i];         
-            
+    if (this.value.length > 0) {
+        for (var i = 0; i < pacientes.length; i++) {
+            var paciente = pacientes[i];
             var tdNome = paciente.querySelector(".info-nome");
-            var nome = tdNome.textContent;       
-            
+            var nome = tdNome.textContent;
             var expressao = new RegExp(this.value, "i");
-            
-            if (expressao.test(nome)) {
-                paciente.classList.remove("invisivel");
-            } else {
+
+            if (!expressao.test(nome)) {
                 paciente.classList.add("invisivel");
-            }        
+            } else {
+                paciente.classList.remove("invisivel");
+            }
         }
     } else {
         for (var i = 0; i < pacientes.length; i++) {
@@ -25,4 +22,4 @@ campoFiltro.addEventListener("input", function(){
             paciente.classList.remove("invisivel");
         }
     }
-})
+});
