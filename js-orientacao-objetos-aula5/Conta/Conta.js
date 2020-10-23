f4015688a0728724b11cc24116320331a4e2d86c
Thiago Henrique Martins da Silva <thiagohenrique1508@gmail.com>
2020-10-22 21:39:42 -0300

    Projeto Final.

--- js-orientacao-objetos-aula5/Conta/Conta.js
+++ js-orientacao-objetos-aula5/Conta/Conta.js
@@ -1,4 +1,14 @@
-export class Conta{
+//Classe abstrata 
+export class Conta {
+    constructor(saldoInicial, cliente, agencia) {
+        if(this.constructor == Conta){
+            throw new Error("Você não deveria instanciar um objeto do tipo Conta Diretamente, pois essa é uma classe abstata");
+        }
+
+        this._saldo = saldoInicial;
+        this._cliente = cliente;
+        this._agencia = agencia;
+    }
 
     set cliente(novoValor) {
         if (novoValor instanceof Cliente) {
@@ -14,34 +24,27 @@ export class Conta{
         return this._saldo;
     }
 
-
-    constructor(saldoInicial, cliente, agencia) {
-        this._saldo = saldoInicial;
-        this._cliente = cliente;
-        this._agencia = agencia;
+    //Método abstrato
+    sacar(valor) {
+       throw new Error("O método Sacar da conta é abstrato")
     }
 
-    sacar(valor) {
-        let taxa = 1;
-      
+    _sacar(valor, taxa){
         const valorSacado = taxa * valor;
         if (this._saldo >= valorSacado) {
             this._saldo -= valorSacado;
             return valorSacado;
         }
+
+        return 0;
     }
 
     depositar(valor) {
-        if (valor <= 100) {
-            return;
-        }
         this._saldo += valor;
     }
 
     tranferir(valor, conta) {
-
         const valorSacado = this.sacar(valor);
         conta.depositar(valorSacado);
-
     }
 }
