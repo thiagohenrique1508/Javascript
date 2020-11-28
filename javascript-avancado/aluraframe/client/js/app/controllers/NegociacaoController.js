6b2378a0fd5c1efd9a2f72ee0b462930b06bcf83
Thiago Henrique Martins da Silva <thiagohenrique1508@gmail.com>
2020-11-28 08:30:07 -0300

    Merge branch 'master' of https://github.com/thiagohenrique1508/Javascript

--- javascript-avancado/aluraframe/client/js/app/controllers/NegociacaoController.js
+++ javascript-avancado/aluraframe/client/js/app/controllers/NegociacaoController.js
@@ -11,20 +11,15 @@ class NegociacaoController{
     adiciona(event){
         event.preventDefault();
 
-        let data = new Date(...
-                this._inputData.value
-                    .split('-')
-                    .map((item, indice) => item - indice % 2)
-        );
-        // console.log(data);
-        // console.log(typeof(this._inputData.value));
+        let data = DateHelper.textoParaData(this._inputData.value);
 
         let negociacao = new Negociacao(
             data,
             this._inputQuantidade.value,
             this._inputValor.value
         );
-        console.log(negociacao);
+
+        
 
     }
 
