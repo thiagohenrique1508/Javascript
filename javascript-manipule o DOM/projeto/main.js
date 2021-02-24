0707fc2c83e83f0bbe8a8d2901d37dbd1fa5ee65
Thiago Henrique Martins da Silva <thiagohenrique1508@gmail.com>
2021-02-24 13:23:01 -0300

    Aula 04 - Formatando a Data

--- javascript-manipule o DOM/projeto/main.js
+++ javascript-manipule o DOM/projeto/main.js
@@ -1,27 +1,29 @@
-import BotaoConclui from './componentes/concluiTarefa.js'
-import BotaoDeleta from './componentes/deletaTarefa.js'
- 
-    const criarTarefa = (evento) => {
+import BotaoConclui from "./componentes/concluiTarefa.js";
+import BotaoDeleta from "./componentes/deletaTarefa.js";
 
-    evento.preventDefault()
+const criarTarefa = (evento) => {
+  evento.preventDefault();
 
-    const lista = document.querySelector('[data-list]')
-    const input = document.querySelector('[data-form-input]')
-    const valor = input.value
+  const lista = document.querySelector("[data-list]");
+  const input = document.querySelector("[data-form-input]");
+  const valor = input.value;
 
-    const tarefa = document.createElement('li')
-    tarefa.classList.add('task')
-    const conteudo = `<p class="content">${valor}</p>`
+  const calendario = document.querySelector("[data-form-date]");
+  const data = moment(calendario.value);
+  //   console.log(data.format("DD/MM/YYYY"));
 
-    tarefa.innerHTML = conteudo
+  const tarefa = document.createElement("li");
+  tarefa.classList.add("task");
+  const conteudo = `<p class="content">${valor}</p>`;
 
-    tarefa.appendChild(BotaoConclui())
-    tarefa.appendChild(BotaoDeleta())
-    lista.appendChild(tarefa)
-    input.value = " "
+  tarefa.innerHTML = conteudo;
 
-}
+  tarefa.appendChild(BotaoConclui());
+  tarefa.appendChild(BotaoDeleta());
+  lista.appendChild(tarefa);
+  input.value = " ";
+};
 
-const novaTarefa = document.querySelector('[data-form-button]')
+const novaTarefa = document.querySelector("[data-form-button]");
 
-novaTarefa.addEventListener('click', criarTarefa)
+novaTarefa.addEventListener("click", criarTarefa);
