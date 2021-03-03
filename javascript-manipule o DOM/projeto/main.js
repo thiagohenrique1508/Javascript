96beb2aac43bcd3f322cf4e62799fcb059a88a9f
Thiago Henrique Martins da Silva <thiagohenrique1508@gmail.com>
2021-03-03 13:26:48 -0300

    Salvando no LocalStorage.

--- javascript-manipule o DOM/projeto/main.js
+++ javascript-manipule o DOM/projeto/main.js
@@ -1,6 +1,8 @@
 import BotaoConclui from "./componentes/concluiTarefa.js";
 import BotaoDeleta from "./componentes/deletaTarefa.js";
 
+let tarefas = [];
+
 const handleNovoItem = (evento) => {
   evento.preventDefault();
 
@@ -15,7 +17,12 @@ const handleNovoItem = (evento) => {
   const dados = { valor, dataFormatada };
   const criaTarefa = criarTarefa(dados);
 
+  tarefas.push(dados);
+
   lista.appendChild(criaTarefa);
+
+  localStorage.setItem("tarefas", JSON.stringify(tarefas));
+
   input.value = " ";
 };
 
