beb1a766137b972cf02778e4f724668fe8e3950b
Thiago Henrique Martins da Silva <thiagohenrique1508@gmail.com>
2021-03-07 09:40:22 -0300

    Projeto Final

--- js_web_1920-aula04/Aula4/main.js
+++
@@ -1,7 +0,0 @@
-import {handleNovoItem} from './componentes/criaTarefa.js'
-import {carregaTarefa} from './componentes/carregaTarefa.js'
-const novaTarefa = document.querySelector('[data-form-button]')
-
-novaTarefa.addEventListener('click', handleNovoItem)
-
-carregaTarefa()
